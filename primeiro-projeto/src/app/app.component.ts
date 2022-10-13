c847c6255d81555bcf6707817cb8abb6fa7a11a5
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-10-13 02:07:57 -0300

    chore: ciclo de vida ngOnChanges()

--- primeiro-projeto/src/app/app.component.ts
+++ primeiro-projeto/src/app/app.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: 
+    //Vai exibir apenas a primeira componente newtitle. Para exibir tudo, deve ser feito a exibição pelo app.component.html 
+  `
+    <app-newtitle newtitle= "Olá mundo"></app-newtitle>
+    <router-outlet></router-outlet>
+  `,
+  
+  //templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 
   //template: '<router-outlet></router-outlet>' -> Simplificado - Apagar (arquivos comp.spec, .html e .scss) e Apagar templateUrl e styleUrls acima.
 })
--- primeiro-projeto/src/app/newtitle/newtitle.component.ts
+++ primeiro-projeto/src/app/newtitle/newtitle.component.ts
@@ -1,25 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-newtitle',
   templateUrl: './newtitle.component.html',
   styleUrls: ['./newtitle.component.scss']
 })
-export class NewtitleComponent implements OnInit {
+export class NewtitleComponent implements OnInit, OnChanges {
   
   //Métodos
   //TS é tipádo, então deve passar o tipo do dado (string, number...)
-  public newtitle: string = "Bem vindo denovo"
+
+  @Input() public newtitle: string = "Bem vindo denovo" //Input para identificar dado externo.
 
   constructor() { }
 
+  //Eventos de ciclo de vida abaixo:
+
   ngOnInit(): void {
-
     console.log('Exemplo de ngOnInit()')
-
     setTimeout(() => {
       console.log('Exemplo de ngOnInit() com mensagem dentro de um setTimeout de 5s')
     }, 5000);
+  }
+
+  ngOnChanges(): void {
+    alert('"Bem vindo denovo" foi alterado para "Olá Mundo"')
   }
 
 }
